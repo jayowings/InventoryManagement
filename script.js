d633baeb6f354574f5c145a354adee004df2707c
Jay Owings <jaydenowings@mail.weber.edu>
2025-09-22 17:34:13 -0600

    add product function added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 let inventory = [];
+
+function addProduct(name, category, quantity, price){
+    let temp = {
+        id : inventory.length,
+        name : name,
+        category : category,
+        quantity : quantity,
+        price : price
+    }
+    if(temp.quantity < 0){
+        console.log("Error: invalid quantity");
+    }
+    else if(temp.price < 0){
+        console.log("Error: invalid price");
+    }else{
+        inventory.push(temp);
+    }
+}
